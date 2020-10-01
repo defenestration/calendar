b22ee37cae09d3e541c5b3124a6b696e9af50f14
Alan Brevick <abrevick@liquidweb.com>
2020-10-01 10:08:08 -0400

    dockerignore

--- db/migrate/20200930211635_add_ldap_bool.rb
+++ db/migrate/20200930211635_add_ldap_bool.rb
@@ -1,5 +1,8 @@
 class AddLdapBool < ActiveRecord::Migration[6.0]
-  def change
-    add_column :users, :ldap, :boolean
+  def up
+    add_column :users, :ldap, :boolean, default: false
+  end
+  def down
+    remove_column :users, :ldap
   end
 end
